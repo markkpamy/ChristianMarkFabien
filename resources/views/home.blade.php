e4b50608e2c22e05882b61c68990a96b5487d1d0
Christian TAGUEJOU <christiantaguejou@yahoo.fr>
2017-03-15 11:17:53 +0100

    ajout de l'extends sur homeblade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
- /* A compléter */
+@extends('layouts.master')
+@section('content')
 <div>
     <h1 class="bvn"> Gestion des oeuvres et de leurs réservations </h1>
 </div>
- /* A compléter */
+@stop
